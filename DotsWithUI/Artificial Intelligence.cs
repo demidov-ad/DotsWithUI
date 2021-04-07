28f0c3509c0bace539352a03eed0f60289621a09
Alexey Demidov <kamillian@bk.ru>
2021-04-07 05:19:52 +0300

    add priority

--- DotsWithUI/Artificial Intelligence.cs
+++ DotsWithUI/Artificial Intelligence.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Net;
 
@@ -10,21 +11,50 @@ namespace DotsWithUI
   {
     private const int CELL_SIZE = 20;
     Random rnd = new Random();
-    
+
+    public List<PointWithPriority> SetPointWithPriority(Field field)
+    {
+      List<PointWithPriority> points = new List<PointWithPriority>();
+      var xyCor = field.TakenAreas;
+      foreach (var elem in xyCor)
+      {
+        foreach (var childElem in elem.Item2)
+        {
+          var pwp = new PointWithPriority(childElem.X, childElem.Y);
+          points.Add(pwp);
+        }
+      }
+      return points;
+    }
+
+    public void ClearPriority(List<PointWithPriority> points)
+    {
+      foreach (var elem in points)
+      {
+        elem.Priority = 0;
+      }
+    }
     
     public void SetPoint(Field field)
     {
-      //var xCor = field.TakenAreas[1].Item2;
-      var p = new Point((int)Math.Round(1f * rnd.Next(100, 500) / CELL_SIZE), 
-        (int)Math.Round(1f * rnd.Next(100,500) / CELL_SIZE));
-      if (field[p] == CellState.Empty)
-      {
-        field.SetPoint(p, CellState.Red);
-      }
-      else
-      {
-        SetPoint(field);
-      }
+      var points = SetPointWithPriority(field);
+      ClearPriority(points);
+      
+
+
+    }
+  }
+
+  public class PointWithPriority
+  {
+    public int X;
+    public int Y;
+    public int Priority;
+    
+    public PointWithPriority(int xSet, int ySet)
+    {
+      this.X = xSet;
+      this.Y = ySet;
     }
   }
 }
